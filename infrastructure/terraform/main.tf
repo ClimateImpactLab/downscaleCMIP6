705e0d739a468e0f11cea035ea93e4e9bcefb2ae
Brewster Malevich <bmalevich@rhg.com>
2021-09-17 11:11:46 -0700

    Upgrade terraform providers
    We need this upgrade for Azure providers, especially, in order to use managed SQL instances.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -9,11 +9,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.41.0"
+      version = "~>2.77.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "1.1.1"
+      version = "~>2.3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~>2.5.0"
     }
   }
 }
--- infrastructure/terraform/modules/kubernetes_internal/main.tf
+++ infrastructure/terraform/modules/kubernetes_internal/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.0.1"
+      version = ">=2.0.1"
     }
   }
 }
