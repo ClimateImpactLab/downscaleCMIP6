8ffaf664319a7856c7c95650f5164423dcdaa9eb
Brewster Malevich <bmalevich@rhg.com>
2021-09-24 16:30:57 -0700

    Add ns, wli resources for new k8s services

--- infrastructure/terraform/gcp-k8s/main.tf
+++ infrastructure/terraform/gcp-k8s/main.tf
@@ -77,6 +77,27 @@ resource "kubernetes_namespace" "argocd" {
 }
 
 
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+      "env"                          = var.env
+    }
+  }
+}
+
+
+resource "kubernetes_namespace" "kubernetes_external_secrets" {
+  metadata {
+    name = "kubernetes-external-secrets"
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+      "env"                          = var.env
+    }
+  }
+}
+
 module "workflows_default_wli" {
   source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   version = "16.1.0"
@@ -104,4 +125,32 @@ resource "kubernetes_secret" "argo-postgres-config" {
     username = data.terraform_remote_state.gcp_core.outputs.argo_cloudsql_username
     password = data.terraform_remote_state.gcp_core.outputs.argo_cloudsql_password
   }
+}
+
+
+module "kubernetes_external_secrets_wli" {
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  version = "16.1.0"
+
+  use_existing_gcp_sa             = true
+  name                            = "kubernetes-external-secrets-grabber"
+  gcp_sa_name                     = data.terraform_remote_state.gcp_core.outputs.kubernetes_external_secrets_account_id
+  namespace                       = kubernetes_namespace.kubernetes_external_secrets.metadata.0.name
+  automount_service_account_token = true
+
+  project_id = var.project_id
+}
+
+
+module "cert_manager_wli" {
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  version = "16.1.0"
+
+  use_existing_gcp_sa             = true
+  name                            = "cert-manager-clouddns"
+  gcp_sa_name                     = data.terraform_remote_state.gcp_core.outputs.cert_manager_account_id
+  namespace                       = kubernetes_namespace.cert_manager.metadata.0.name
+  automount_service_account_token = true
+
+  project_id = var.project_id
 }
