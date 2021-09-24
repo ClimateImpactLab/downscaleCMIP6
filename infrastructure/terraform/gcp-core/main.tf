d456b7669af058a6c159fcc75ba2809bc53ec2b1
Brewster Malevich <bmalevich@rhg.com>
2021-09-24 11:20:22 -0700

    Teardown to move to new region

--- infrastructure/terraform/gcp-core/main.tf
+++ infrastructure/terraform/gcp-core/main.tf
@@ -32,19 +32,19 @@ module "artifact_registry" {
   env        = var.env
 }
 
-module "datalake_storage" {
-  source = "./modules/datalake_storage"
-
-  sql_vpc_id = module.networking.vpc_id
-  sql_region = var.region
-
-  project_id = var.project_id
-  location   = var.region
-  env        = var.env
-
-  # The cloudsql instance depends on the VPC network connection being set up.
-  depends_on = [module.networking]
-}
+#module "datalake_storage" {
+#  source = "./modules/datalake_storage"
+#
+#  sql_vpc_id = module.networking.vpc_id
+#  sql_region = var.region
+#
+#  project_id = var.project_id
+#  location   = var.region
+#  env        = var.env
+#
+#  # The cloudsql instance depends on the VPC network connection being set up.
+#  depends_on = [module.networking]
+#}
 
 
 module "networking" {
@@ -74,48 +74,48 @@ module "least_privileged_gke_sa" {
 }
 
 
-module "gke_argo_cluster" {
-  source = "./modules/gke_argo_cluster"
+#module "gke_argo_cluster" {
+#  source = "./modules/gke_argo_cluster"
+#
+#  cluster_name_prefix = "tsukuyomi"
+#
+#  network                       = module.networking.vpc_id
+#  subnetwork                    = module.networking.publicsubnet_id
+#  cluster_secondary_range_name  = module.networking.pod_range_name
+#  services_secondary_range_name = module.networking.services_range_name
+#
+#  node_service_account_email = module.least_privileged_gke_sa.email
+#
+#  env        = var.env
+#  project_id = var.project_id
+#  region     = var.region
+#  zone       = var.zone
+#}
 
-  cluster_name_prefix = "tsukuyomi"
-
-  network                       = module.networking.vpc_id
-  subnetwork                    = module.networking.publicsubnet_id
-  cluster_secondary_range_name  = module.networking.pod_range_name
-  services_secondary_range_name = module.networking.services_range_name
-
-  node_service_account_email = module.least_privileged_gke_sa.email
-
-  env        = var.env
-  project_id = var.project_id
-  region     = var.region
-  zone       = var.zone
-}
-
-
-resource "google_service_account" "workflows_default" {
-  account_id   = "workflows-default"
-  description  = "Default Argo Workflow default worker service account"
-  display_name = "workflows-default"
-}
-resource "google_storage_bucket_iam_member" "argoworker_buckets_iammember" {
-  for_each = toset(
-    [
-      module.datalake_storage.raw_cmip6_bucket_name,
-      module.datalake_storage.clean_cmip6_bucket_name,
-      module.datalake_storage.raw_reanalysis_bucket_name,
-      module.datalake_storage.clean_reanalysis_bucket_name,
-      module.datalake_storage.biascorrected_bucket_name,
-      module.datalake_storage.biascorrected_stage_bucket_name,
-      module.datalake_storage.downscaled_bucket_name,
-      module.datalake_storage.downscaled_stage_bucket_name,
-      module.datalake_storage.qualitycontrol_bucket_name,
-      module.datalake_storage.qualitycontrol_stage_bucket_name,
-      module.datalake_storage.scratch_bucket_name,
-      module.datalake_storage.support_bucket_name
-    ]
-  )
-  bucket = each.key
-  member = "serviceAccount:${google_service_account.workflows_default.email}"
-  role   = "roles/storage.admin"
-}
+#
+#resource "google_service_account" "workflows_default" {
+#  account_id   = "workflows-default"
+#  description  = "Default Argo Workflow default worker service account"
+#  display_name = "workflows-default"
+#}
+#resource "google_storage_bucket_iam_member" "argoworker_buckets_iammember" {
+#  for_each = toset(
+#    [
+#      module.datalake_storage.raw_cmip6_bucket_name,
+#      module.datalake_storage.clean_cmip6_bucket_name,
+#      module.datalake_storage.raw_reanalysis_bucket_name,
+#      module.datalake_storage.clean_reanalysis_bucket_name,
+#      module.datalake_storage.biascorrected_bucket_name,
+#      module.datalake_storage.biascorrected_stage_bucket_name,
+#      module.datalake_storage.downscaled_bucket_name,
+#      module.datalake_storage.downscaled_stage_bucket_name,
+#      module.datalake_storage.qualitycontrol_bucket_name,
+#      module.datalake_storage.qualitycontrol_stage_bucket_name,
+#      module.datalake_storage.scratch_bucket_name,
+#      module.datalake_storage.support_bucket_name
+#    ]
+#  )
+#  bucket = each.key
+#  member = "serviceAccount:${google_service_account.workflows_default.email}"
+#  role   = "roles/storage.admin"
+#}
