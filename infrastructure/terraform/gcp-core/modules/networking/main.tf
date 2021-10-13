8d8ee7e08c19e29b3adac5d8f56b108c224f9044
Brewster Malevich <bmalevich@rhg.com>
2021-10-13 15:52:35 -0700

    Remove cruft internal default-allow VPC firewall rule

--- infrastructure/terraform/gcp-core/modules/networking/main.tf
+++ infrastructure/terraform/gcp-core/modules/networking/main.tf
@@ -68,27 +68,3 @@ resource "google_compute_firewall" "iap" {
     "35.235.240.0/20",
   ]
 }
-
-resource "google_compute_firewall" "default-allow-internal" {
-  name     = "${var.company}-${var.application}-${var.env}-internal-allow"
-  network  = google_compute_network.vpc1.name
-  priority = 65534
-  source_ranges = [
-    var.subnet_range,
-  ]
-  direction = "INGRESS"
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["0-65535"]
-  }
-
-  allow {
-    protocol = "udp"
-    ports    = ["0-65535"]
-  }
-}
