7e4be96cb076511740aabed1fd066453bdeb5a8e
Brewster Malevich <bmalevich@rhg.com>
2021-11-30 13:35:42 -0800

    Scale up core machines in prep for big runs

--- infrastructure/terraform/gcp-core/modules/gke_argo_cluster/main.tf
+++ infrastructure/terraform/gcp-core/modules/gke_argo_cluster/main.tf
@@ -82,7 +82,7 @@ resource "google_container_node_pool" "core" {
   }
 
   node_config {
-    machine_type    = "e2-medium"
+    machine_type    = "e2-standard-2"
     image_type      = "COS_CONTAINERD"
     service_account = var.node_service_account_email
     oauth_scopes = [
