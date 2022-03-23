558c12599c988008410ddeee12b49da7911ae450
Diana <dgergel@gmail.com>
2022-03-23 12:16:54 -0700

    remove organized notebooks

--- notebooks/downscaling_pipeline/regridding.py
+++
@@ -1,9 +0,0 @@
-import xesmf as xe
-import pandas as pd
-import xarray as xr
-
-def apply_weights(regridder, input_data):
-    regridder._grid_in = None
-    regridder._grid_out = None
-    result = regridder(input_data)
-    return result
