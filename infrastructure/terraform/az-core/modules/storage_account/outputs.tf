d05de120277158121499f2921d60d0bed3ea5d2b
Brewster Malevich <bmalevich@rhg.com>
2022-10-25 13:00:45 -0700

    Drop Azure Terraform resources
    We are no longer using Azure for cloud infrastructure, so completely removing this.

--- infrastructure/terraform/az-core/modules/storage_account/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "name" {
-  value = azurerm_storage_account.storageacc.name
-}
-
-output "primary_access_key" {
-  value     = azurerm_storage_account.storageacc.primary_access_key
-  sensitive = true
-}
